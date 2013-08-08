b58dee717a1d8fcb954a44393c803f1ccb2344da
Toby Crawley <toby@tcrawley.org>
2013-08-08 19:44:32 -0400

    Bump version to 0.1.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.immutant/immutant-dependency-exclusions "0.1.0"
+(defproject org.immutant/immutant-dependency-exclusions "0.1.1-SNAPSHOT"
   :description "A tiny util for excluding Immutant dependencies from Leiningen dependency coordinates"
   :url "https://github.com/immutant/immutant-dependency-exclusions"
   :license {:name "Eclipse Public License"
